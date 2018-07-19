0a57ccf0b7c1417dc606f75e3bb943a7900625ff
Aj <adtune6509@student.waynecc.edu>
2018-07-18 20:14:33 -0400

    Update KoalaTools.py

--- KoalaTools.py
+++ KoalaTools.py
@@ -1,5 +1,13 @@
+import re
+
+# Detects if the message is a greeting
 def isGreeting(message):
     if message.content.lower().startswith('hello') or message.content.lower().startswith('hi') or message.content.lower().startswith('hey'):
         return True
     else:
         return False
+
+# Removes a substring from a string, (really me just testing out my knowledge of Regular Expressions)
+def removeStr(str, val):
+    regexp = r'[^{0}]+'.format(val)
+    return re.findall(regexp, str);
