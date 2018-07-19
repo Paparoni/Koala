34b631ea7ac16ad7e3d9f37c8a5907a0b4ee3f87
Aj <adtune6509@student.waynecc.edu>
2018-07-19 14:36:46 -0400

    Update KoalaTools.py

--- KoalaTools.py
+++ KoalaTools.py
@@ -9,5 +9,10 @@ def isGreeting(message):
 
 # Removes a substring from a string, (really me just testing out my knowledge of Regular Expressions)
 def removeStr(str, val):
-    regexp = r'[^{0}]+'.format(val)
-    return re.findall(regexp, str);
+    regexp = r'[^{0}^[ ]+'.format(val)
+    return re.findall(regexp, str)[0];
+
+Drama = [
+    '{0} said {1} killed their cat and sold the fur to a homeless man to use as toilet paper.',
+    '{0} stole $5000 worth of custom Roblox porn art books from {1}\'s secret drawer :0'
+]
