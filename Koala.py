63b4f5f8348cb2e155d3a245009af231815fdf82
Aj <adtune6509@student.waynecc.edu>
2018-07-18 21:07:41 -0400

    Update Koala.py

--- Koala.py
+++ Koala.py
@@ -16,8 +16,8 @@ Koala = discord.Client()
 # Symbol that notifies the bot that the message is a command.
 commandIdentifier = "$"
 
+@Koala.event
 async def on_message(message):
-
 # Checks if the message came from Koala himself, if so, do nothing. :)
     if message.author == Koala.user:
         return
