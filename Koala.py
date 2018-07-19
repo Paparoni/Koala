aa4484dadec3f2e6a98b87b526f402187f0521f9
Aj <adtune6509@student.waynecc.edu>
2018-07-18 21:05:53 -0400

    Update Koala.py

--- Koala.py
+++ Koala.py
@@ -16,10 +16,6 @@ Koala = discord.Client()
 # Symbol that notifies the bot that the message is a command.
 commandIdentifier = "$"
 
-@Koala.event
-async def on_ready():
-    print('Logged in')
-
 async def on_message(message):
 
 # Checks if the message came from Koala himself, if so, do nothing. :)
@@ -29,27 +25,7 @@ async def on_message(message):
 # Detects if the message is a greeting and just says hello back
     if Tools.isGreeting(message):
         msg_txt = 'Hello {0.author.mention}'.format(message)
-        await Koala.send_message(message.channel, msg_txt)
-
-
-# Start Command Stream
-    if message.startswith(commandIdentifier):
-        # Get into the nitty gritty
-
-        # Removes the command identifier from the string, easier that way.
-        command = Tools.removeStr(message, commandIdentifier)
-
-
-        # Gets the text after the spaces for the command purpose
-        command_index_1 = message.split(' ')[1]
-        command_index_2 = message.split(' ')[2]
-        command_index_3 = message.split(' ')[3]
-
-        # Now this is where we start writing the commands.
-        # Unfortunately this part gets messy because of Python's lack of case/switch
-        if command == 'jump':
-            msg_txt = 'How High?'
-            await Koala.send_message(message.channel, msg_txt)
+        await Koala.send_message(message.channel, msg_txt
 
 @Koala.event
 async def on_ready():
