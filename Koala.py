d8253e7ce7a8bdeffcbf29dcb76eded155913ae3
Aj <adtune6509@student.waynecc.edu>
2018-07-20 13:47:15 -0400

    Update Koala.py

--- Koala.py
+++ Koala.py
@@ -9,13 +9,6 @@ import shutil
 import os
 import socket
 
-HOST = ''                 # Symbolic name meaning the local host
-PORT = os.environ['PORT']              
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((HOST, PORT))
-s.listen(1)
-
-
 Koala = discord.Client()
 commandIdentifier = "$"
 @Koala.event
