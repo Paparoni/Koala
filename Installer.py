ed6972e0afc0bc17930f97addbf2592a3d5db23a
Aj <adtune6509@student.waynecc.edu>
2018-07-20 23:29:00 -0400

    Update Installer.py

--- Installer.py
+++ Installer.py
@@ -1,6 +1,15 @@
 import subprocess
 import sys
+import os
 
 subprocess.call([sys.executable, '-m', 'pip', 'install', 'discord.py'])
 subprocess.call([sys.executable, '-m', 'pip', 'install', 'requests'])
 subprocess.call([sys.executable, '-m', 'pip', 'install', 'Pillow'])
+
+cwd = os.getcwd()
+bat = open("Koala.bat","w+")
+bat.write("cd {}\n".format(cwd))
+bat.write("py Koala.py")
+bat.close()
+
+print("Koala successfully installed. \nPlease run Koala.bat to get started.")
