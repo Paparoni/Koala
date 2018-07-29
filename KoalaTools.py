b3220b73c0cd1101f9aadf1f1c4662978029debe
Aj <adtune6509@student.waynecc.edu>
2018-07-29 15:00:32 -0400

    file handling functions

--- KoalaTools.py
+++ KoalaTools.py
@@ -1,4 +1,5 @@
 import re
+import time
 
 # Detects if the message is a greeting
 def isGreeting(message):
@@ -16,3 +17,21 @@ Drama = [
     '{0} said {1} killed their cat and sold the fur to a homeless man to use as toilet paper.',
     '{0} stole $5000 worth of custom Roblox porn art books from {1}\'s secret drawer :0'
 ]
+
+def saveTextFile(data, name):
+    f = open(name, "w+")
+    if type(data) is list:
+        for i in data:
+            f.write(i+"\n")
+    else:
+        f.write(data)
+    f.close()
+
+def updateTextFile(data, name):
+    f = open(name, "a+")
+    f.write(data)
+    f.close()
+
+def saveMessageLog(messages):
+    date = time.strftime("%m-%d-%y", time.localtime())
+    saveTextFile(messages, date+"_messagelog.txt")
